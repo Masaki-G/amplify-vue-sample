e4573d3b6b7fa7e5ee33358d165f6b6d77a32e92
masaki12345 <masasaki3107@gmail.com>
2022-10-19 18:48:05 +0900

    first commit

---
+++ src/test.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import { I18n } from "aws-amplify";
+import { translations } from "@aws-amplify/ui-vue";
+I18n.putVocabularies(translations);
+I18n.setLanguage("ja");
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
+
